694bc5ea361c53c3192c749885e4cdb7bfdd9780
ian-mcr <115840092+ian-mcr@users.noreply.github.com>
2025-02-09 16:32:52 +0000

    Add files via upload

--- birthdayanimtion.py
+++ birthdayanimtion.py
@@ -7,13 +7,32 @@ screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption(TITLE)
 
 bg1=pygame.image.load("party room.jpg")
-
+bg2=pygame.image.load("party cake.jpg")
+bg3=pygame.image.load("present.jpg")
 run=True
 while run:
     screen.blit(bg1,(0,0))
     font=pygame.font.SysFont("Arial",50)
     text=font.render("happy birthday",True,"red")
-    screen.blit(text,(300,300))
+    screen.blit(text,(180,270))
+    pygame.display.update()
+    pygame.time.delay(1000)
+
+    screen.blit(bg2,(0,0))
+    font=pygame.font.SysFont("times new roman",50)
+    text=font.render("make a wish",True,"red")
+    screen.blit(text,(180,50))
+    pygame.display.update()
+    pygame.time.delay(1000)
+
+    screen.blit(bg3,(0,0))
+    font=pygame.font.SysFont("verdana",45)
+    text=font.render("open the the presents",True,"orange")
+    screen.blit(text,(60,50))
+    pygame.display.update()
+    pygame.time.delay(1000)
+
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
